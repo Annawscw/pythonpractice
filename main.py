e0e3d8c6306a21197b7a64ea9934ace1df95e462
Annawscw <159732983+Annawscw@users.noreply.github.com>
2024-05-07 15:06:08 +1200

    changes

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+score = 0
 # ask the user their name and store it 
 name = input ("what is your name?")
 # greet user and interduce quiz
 print ("Hello welcome to the quizz ", name)
+
 # ask the user a question and get their response
 answer = input ("do you think cats are cute?")
 # Tell them the correct answer
-print ("the correct answer is ", answer)
+print ("The correct answer is ", answer)
+
 # a question 
-answer = input (" In 1963 did a cat go to space?")
+answer = input (" In 1963 did a cat go to space? ")
 # answer to the question
-print ("they did",answer)
+if answer == "Correct":
+    print("Correct")
+    score += 5
+elif answer == "":
+    print ("I don't know")
+else :
+    print("Incorrect!")
+    print ("The answer was correct")
 # the end
-input ("goodbye")
+print ("Goodjob you got ", score, "pionts" "goodbye")
+
