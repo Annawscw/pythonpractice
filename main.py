efdb034675e3e4809c729be76532d8f6313d10cb
Annawscw <159732983+Annawscw@users.noreply.github.com>
2024-05-13 11:59:46 +1200

    changes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 score = 0
+QUESTION_FORMAT ="{}\na.{} b.{} c.{} d.{}"
+
 # ask the user their name and store it 
 name = input ("what is your name?")
 # greet user and interduce quiz
@@ -15,7 +17,6 @@ elif answer == "":
 else :
     print("Incorrect!")
     print ("The answer was correct")
-
 # a question 
 answer = input (" In 1963 did a cat go to space? ") .lower()
 # answer to the question
@@ -27,6 +28,19 @@ elif answer == "":
 else :
     print("Incorrect!")
     print ("The answer was correct")
+
+# multi question 
+
+question = "what bread of cat is the longest?"
+a = "Ragdoll"
+b = "Maine Coon"
+c = "British Shorthair"
+d = "Scottish Fold"
+answer = input(QUESTION_FORMAT.format(question,a, b, c, d,)) .lower()
+if answer == "b" .lower() or answer == b. lower():
+
 # the end
-print ("Goodjob {} got {} pionts.\ngood-bye" .format(name, score))
+print ("Goodjob {} you got {} pionts.\ngood-bye" .format(name, score))
+
+
 
